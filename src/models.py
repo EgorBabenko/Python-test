7bb07b63a4eb40bd5495d4725d40fe14cbc81ec4
Егор Бабенко <ipbabenko72@gmail.com>
2022-09-15 17:22:01 +0500

    fix picnic_add  and  add register_to_picnic

--- src/models.py
+++ src/models.py
@@ -1,4 +1,6 @@
+from datetime import datetime as dt
 from pydantic import BaseModel
+from sqlalchemy.orm.collections import InstrumentedList
 
 
 class RegisterUserRequest(BaseModel):
@@ -15,3 +17,23 @@ class UserModel(BaseModel):
 
     class Config:
         orm_mode = True
+
+
+class RegisterPicnicModel(BaseModel):
+    city_id: int
+    datetime: dt
+
+
+class PicnicModel(BaseModel):
+    id: int
+    city_id: int
+    time: dt
+    users: InstrumentedList
+
+    class Config:
+        orm_mode = True
+
+
+class UserPicnicRegistration(BaseModel):
+    user_id: int
+    picnic_id: int
--- src/routers/picnics.py
+++ src/routers/picnics.py
@@ -1,6 +1,7 @@
 import datetime as dt
-from fastapi import APIRouter, Query
-from database import Session, Picnic, PicnicRegistration, City
+from fastapi import APIRouter, Query, HTTPException
+from database import Session, Picnic, PicnicRegistration, City, User
+from models import RegisterPicnicModel, PicnicModel, UserPicnicRegistration
 
 picnics_router = APIRouter(prefix='/picnics')
 
@@ -19,40 +20,43 @@ def all_picnics(datetime: dt.datetime = Query(default=None,
     if not past:
         picnics = picnics.filter(Picnic.time >= dt.datetime.now())
 
-    return [{
-        'id': pic.id,
-        'city': Session().query(City).filter(City.id == pic.id).first().name,
-        'time': pic.time,
-        'users': [
-            {
-                'id': pr.user.id,
-                'name': pr.user.name,
-                'surname': pr.user.surname,
-                'age': pr.user.age,
-            }
-            for pr in Session().query(PicnicRegistration).filter(
-                PicnicRegistration.picnic_id == pic.id)],
-    } for pic in picnics]
+    return [PicnicModel.from_orm(picnic_object) for picnic_object in picnics]
 
 
 @picnics_router.post('/', summary='Picnic Add', tags=['picnic'])
-def picnic_add(city_id: int = None, datetime: dt.datetime = None):
-    p = Picnic(city_id=city_id, time=datetime)
+def picnic_add(picnic: RegisterPicnicModel):
+    picnic_object = Picnic(city_id=picnic.city_id, time=picnic.datetime)
     s = Session()
-    s.add(p)
+    s.add(picnic_object)
     s.commit()
-
-    return {
-        'id': p.id,
-        'city': Session().query(City).filter(City.id == p.id).first().name,
-        'time': p.time,
-    }
+    return PicnicModel.from_orm(picnic_object)
 
 
 @picnics_router.post('/usersignup/', summary='Picnic Registration', tags=['picnic'])
-def register_to_picnic(*_, **__, ):
-    """
-    Регистрация пользователя на пикник
-    (Этот эндпойнт необходимо реализовать в процессе выполнения тестового задания)
-    """
-    return ...
+def register_to_picnic(data: UserPicnicRegistration):
+    session = Session()
+    user = session.query(User).filter(User.id == data.user_id).first()
+    if not user:
+        raise HTTPException(status_code=400,
+                            detail='Пользователя с этим id не существует')
+    picnic = session.query(Picnic).filter(Picnic.id == data.picnic_id).first()
+    if not picnic:
+        raise HTTPException(status_code=400,
+                            detail='Пикника с этим id не существует')
+
+    repeat = session.query(PicnicRegistration).filter(PicnicRegistration.picnic_id == picnic.id,
+                                                      PicnicRegistration.user_id == user.id).first()
+
+    if repeat:
+        raise HTTPException(status_code=400,
+                            detail='Пользователь уже зарегестрирован')
+
+    registration = PicnicRegistration(user_id=user.id,
+                                      picnic_id=picnic.id)
+
+    session.add(registration)
+    session.commit()
+
+    return {'user': f'{user.name} {user.surname}',
+            'picnic': picnic.id,
+            'message': 'Визит на пикник зарегестрирован'}
